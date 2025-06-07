4f8715ca1bd160495d0f46036ed4a5f4b0ef2d04
Georgina Kimani <njokikimani001@gmail.com>
2025-06-07 22:29:19 +0300

    added url

--- drinks_backend/drinks_backend/settings.py
+++ drinks_backend/drinks_backend/settings.py
@@ -14,7 +14,8 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'drinks-backend-r99k.onrender.com',
     'barrush-backend.onrender.com',
-    'localhost'
+    'localhost',
+    '127.0.0.1',
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -57,7 +58,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://barrush.netlify.app",
     "https://barrush.co.ke",
     "http://localhost:8080",
-    "http://localhost:3000",
+    "http://127.0.0.1:8000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
